4237122a1adb4bdc8bd5fedd2f012e5b3997cbd4
Ismo Aro <ismo.aro@gmail.com>
2023-12-04 07:01:10 +0200

    renamed the file

--- test_energyMeter.py
+++ test_energyMeter.py
@@ -1,17 +1,17 @@
 import unittest
 from unittest.mock import patch, mock_open
-from energyCalculator import energyCalculator
+from energyMeter import energyMeter
 import json
 
-class TestEnergyCalculator(unittest.TestCase):
+class TestEnergyMeter(unittest.TestCase):
     def setUp(self):
-        self.energy_calculator = energyCalculator()
+        self.energy_meter = energyMeter()
 
     def test_load_consumption_data_success(self):
         mock_data = '{"processor": {"i7-8650U": 50}, "ram": {"ddr4": 2}, "network": 1}'
         with patch('builtins.open', mock_open(read_data=mock_data)):
-            self.energy_calculator.load_consumption_data()
-            self.assertEqual(self.energy_calculator.consumption_data['processor']['i7-8650U'], 50)
+            self.energy_meter.load_consumption_data()
+            self.assertEqual(self.energy_meter.consumption_data['processor']['i7-8650U'], 50)
 
 if __name__ == '__main__':
     unittest.main()

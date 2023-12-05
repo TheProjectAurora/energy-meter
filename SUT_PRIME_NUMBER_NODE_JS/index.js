2d866eb26bc2df8f3e943a694a393ef69a539a98
Nina Ahlberg <nina.ahlberg@gmail.com>
2023-12-05 09:43:44 +0200

    prime number calculator

--- SUT_PRIME_NUMBER_NODE_JS/index.js
+++ SUT_PRIME_NUMBER_NODE_JS/index.js
@@ -3,7 +3,121 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
-let lastPrime = 2;
+let globalLastPrime = 2; // Keep track of the last prime number globally
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+    res.send(`
+    <html>
+      <head>
+        <title>Simple Node.js Page</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <script>
+          let primeCount = 0;
+          let primeNumbers = [];
+          let lastPrime = 1;
+
+          document.addEventListener('DOMContentLoaded', () => {
+              fetch('/currentPrime')
+                  .then(response => response.json())
+                  .then(data => {
+                      lastPrime = data.currentPrime - 1;
+                  })
+                  .catch(error => console.error('Error:', error));
+          });
+
+          function sendCalculateRequest() {
+            const count = document.getElementById('primeNumberInput').value;
+            fetch('/calculate?start=2&count=' + count)
+              .then(response => response.text())
+              .then(data => {
+                const container = document.getElementById('primeContainer');
+                container.innerText = data;
+                container.style.display = 'block';
+              })
+              .catch(error => console.error('Error:', error));
+          }
+
+          function requestNextPrime() {
+            lastPrime = 1; // Reset lastPrime to 1 so that the next prime starts from 2
+            fetchNextPrime();
+          }
+
+          function fetchNextPrime() {
+            const totalCount = parseInt(document.getElementById('primeNumberInput').value, 10);
+            if (primeCount >= totalCount) {
+                return;
+            }
+            fetch('/nextPrime?lastPrime=' + lastPrime)
+                .then(response => response.text())
+                .then(data => {
+                    const nextPrime = data.split(': ')[1];
+                    primeNumbers.push(nextPrime);
+                    lastPrime = parseInt(nextPrime, 10);
+                    const container = document.getElementById('primeContainer');
+                    container.innerText = primeNumbers.join(', ');
+                    primeCount++;
+                    if (primeCount < totalCount) {
+                        fetchNextPrime();
+                    }
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    document.getElementById('primeContainer').innerText = 'Error fetching prime number';
+                });
+          }
+
+          function reset() {
+            primeCount = 0;
+            primeNumbers = [];
+            lastPrime = 1;
+            document.getElementById('primeContainer').innerText = 'Prime numbers will appear here';
+            fetch('/reset?lastPrime=' + lastPrime)
+                .catch(error => console.error('Error:', error));
+          }
+        </script>
+      </head>
+      <body>
+        <div class="content-container">
+          <div class="inputs-buttons-container">
+            <div class="input-container">
+              <label for="primeNumberInput">Prime numbers:</label>
+              <input type="number" id="primeNumberInput" value="5000" min="1" />
+            </div>
+            <button id="calculateButton" onclick="sendCalculateRequest()">Calculate prime numbers</button>
+            <button id="nextPrimesButton" onclick="requestNextPrime()">Request the next prime number</button>
+            <button id="resetButton" onclick="reset()">C</button>
+          </div>
+          <div id="primeContainer">Prime numbers will appear here</div>
+        </div>
+      </body>
+    </html>
+  `);
+});
+
+app.get('/calculate', (req, res) => {
+    const start = parseInt(req.query.start, 10);
+    const count = parseInt(req.query.count, 10);
+    const primes = calculatePrime(start, count);
+    res.send(primes.join(', '));
+});
+
+app.get('/nextPrime', (req, res) => {
+    let lastPrime = parseInt(req.query.lastPrime, 10) || globalLastPrime;
+    const nextPrime = calculatePrime(lastPrime + 1, 1)[0];
+    globalLastPrime = nextPrime;
+    res.send(`The next prime number is: ${nextPrime}`);
+});
+
+app.get('/reset', (req, res) => {
+    globalLastPrime = 2;
+    res.send('Reset successful');
+});
+
+app.get('/currentPrime', (req, res) => {
+    res.send({ currentPrime: globalLastPrime });
+});
 
 function calculatePrime(start, count) {
     let primes = [];
@@ -24,77 +138,6 @@ function calculatePrime(start, count) {
     }
     return primes;
 }
-
-app.use(express.static(path.join(__dirname)));
-
-app.get('/', (req, res) => {
-    res.send(`
-    <html>
-      <head>
-        <title>Simple Node.js Page</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
-        <script>
-          let primeCount = 0;
-
-          function sendCalculateRequest() {
-            fetch('/calculate?start=2&count=5000')
-              .then(response => response.text())
-              .then(data => {
-                const container = document.getElementById('primeContainer');
-                container.innerText = data;
-                container.style.display = 'block';
-                document.getElementById('calculateButton').style.display = 'none';
-              })
-              .catch(error => console.error('Error:', error));
-          }
-
-          function requestNextPrime() {
-              if (primeCount >= 5000) {
-                  document.getElementById('primeContainer').innerText = 'Reached 5000 prime numbers!';
-                  return;
-              }
-
-              document.getElementById('primeContainer').innerText = \`Requesting prime number \${primeCount + 1}...\`;
-
-              setTimeout(() => {
-                  fetch('/nextPrime')
-                      .then(response => response.text())
-                      .then(data => {
-                          document.getElementById('primeContainer').innerText = data;
-                          primeCount++;
-                          requestNextPrime();
-                      })
-                      .catch(error => {
-                          console.error('Error:', error);
-                          document.getElementById('primeContainer').innerText = 'Error fetching prime number';
-                      });
-              }, 2000);
-          }
-        </script>
-      </head>
-      <body>
-        <div class="content-container">
-          <button id="calculateButton" onclick="sendCalculateRequest()">Calculate the first 5000 prime numbers</button>
-          <button id="nextPrimesButton" onclick="requestNextPrime()">Request the next prime number</button>
-          <div id="primeContainer"></div>
-        </div>
-      </body>
-    </html>
-  `);
-});
-
-app.get('/calculate', (req, res) => {
-    const start = parseInt(req.query.start, 10);
-    const count = parseInt(req.query.count, 10);
-    const primes = calculatePrime(start, count);
-    res.send(`Prime Numbers:\n\n${primes.join(', ')}`);
-});
-
-app.get('/nextPrime', (req, res) => {
-    const nextPrime = calculatePrime(lastPrime + 1, 1)[0];
-    lastPrime = nextPrime;
-    res.send(`The next prime number is: ${nextPrime}`);
-});
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
